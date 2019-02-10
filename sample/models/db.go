bc851c15cd590163c7dd198532d9f5590ec86a74
gomzaemon111 <goki14914@icloud.com>
2019-02-11 01:30:22 +0900

    add for hatenablog gorm

--- sample/models/db.go
+++ sample/models/db.go
@@ -1,6 +1,7 @@
 package models
 
 // https://qiita.com/chan-p/items/cf3e007b82cc7fce2d81
+// https://blog.daimori.com/entry/gin-gorm%25e3%2581%25a7%25e7%25b0%25a1%25e5%258d%2598%25e3%2581%25aaweb%25e3%2582%25a2%25e3%2583%2597%25e3%2583%25aa%25e3%2582%2592%25e4%25bd%259c%25e3%2581%25a3%25e3%2581%25a6%25e3%2581%25bf%25e3%2581%259f
 
 import (
 	"github.com/go-sql-driver/mysql"
