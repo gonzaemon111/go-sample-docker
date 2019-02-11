920306497e2379c36626210c5be9982c1a6bf020
gomzaemon111 <goki14914@icloud.com>
2019-02-11 21:40:45 +0900

    main.go

--- sample/main.go
+++ sample/main.go
@@ -1,10 +1,25 @@
 package main
 
 import(
+  "os"
+  "log"
+  "fmt"
   "github.com/gin-gonic/gin"
+  "github.com/joho/godotenv"
 )
 
+func Env_load() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
+
+
 func main() {
+    Env_load()
+    message := fmt.Sprintf("user_name=%s password=%s", os.Getenv("USER_NAME"), os.Getenv("PASSWORD"))
+    fmt.Println(message)
     r := gin.Default()
     r.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{
